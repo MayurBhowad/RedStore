e6f22cffecf519dc32fdfaf6eb919bba82f26c99
MAYUR_M_BHOWAD <mayurbhowad1995@gmail.com>
2023-02-25 17:40:35 +0530

    fix: express static folder

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ app.use("/api/payment/", require("./routes/api/payment.routes"));
 //serve static assets if in Production
 // if (process.env.NODE_ENV === 'production') {
 //set static folder
-app.use(express.static("client/build"));
+// app.use(express.static("client/build"));
+app.use(express.static("build"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
